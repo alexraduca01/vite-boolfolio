4221ae3b9dde15fc39951167f5e093022d3e91db
alexraduca01 <alex.raduca01@icloud.com>
2024-01-22 10:16:30 +0100

    added router

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,28 @@
   <div>
     <h1>Hello world</h1>
   </div>
+  <router-view></router-view>
 </template>
 
 <script>
+import axios from "axios";
+import { store } from "./store";
   export default {
     name: 'App',
     data(){
       return {
-        
+        store,
       }
+    },
+    methods: {
+      getApiTest(){
+        axios.get(this.store.apiUrl + "projects").then((res) => {
+          console.log(res.data);
+        })
+      }
+    },
+    mounted() {
+      this.getApiTest();
     }
   }
 </script>
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>AppHome</h2>
+    </div>
+</template>
+
+<script>
+import { store } from "../store";
+    export default {
+        name: 'AppHome',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
