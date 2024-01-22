67a52544856e98505731a6c4ae96ad9a679e0003
alexraduca01 <alex.raduca01@icloud.com>
2024-01-22 09:55:44 +0100

    did installations

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@use './assets/styles/partials/variables' as *;
+
+h1 {
+    color: $font-color;
+}
+
+</style>
